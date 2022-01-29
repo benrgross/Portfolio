1de4f4bbccbf82d1ba670780d723824db1609ce6
Ben Gross <benrgross@gmail.com>
2022-01-29 16:09:04 -0500

    contact page added

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { ContactUs } from "../components/ContactUs";
+import { Container } from "react-bootstrap";
 
 function Contact() {
-  return <div>contact</div>;
+  return (
+    <div>
+      <Container className="contact_callout">
+        <h3>Contact Me</h3>
+        <h4>
+          Please reach out to me with any inqueries and I will respond within 1
+          to 2 days!
+        </h4>
+      </Container>
+      <ContactUs />
+    </div>
+  );
 }
 
 export default Contact;
