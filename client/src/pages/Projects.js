f95bb2c32404f6fd5650305a4c22891902c2312d
Ben Gross <benrgross@gmail.com>
2022-01-30 18:11:31 -0500

    added trowel photos

--- client/src/pages/Projects.js
+++ client/src/pages/Projects.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
 function Projects() {
-  return <div>projects</div>;
+  return (
+    <div>
+      <Container className="projects__cont">
+        <h2 className="projects__h2">Projects</h2>
+        <Row>
+          <Col md={6}>
+            <div className="projects__media">
+              <img className="projects__img" src="/img/trowel-snapshot-2" />
+            </div>
+          </Col>
+          <Col></Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
 export default Projects;
