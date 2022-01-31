a0d0d7c33a83aba4209f5c57171a17c373adb2d2
Ben Gross <benrgross@gmail.com>
2022-01-31 10:19:04 -0500

    onclick button and state change for projects implimented

--- client/src/pages/Projects.js
+++ client/src/pages/Projects.js
@@ -1,41 +1,62 @@
-import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import React, { useState } from "react";
+import { Container, Row, Col, Button } from "react-bootstrap";
 
 function Projects() {
+  const [spotlight, setSpotlight] = useState(true);
+
+  const goToProject = () => {
+    console.log("click");
+    setSpotlight(false);
+  };
+
   return (
     <div>
-      <Container className="projects__cont">
-        <h2 className="projects__h2">Projects</h2>
-        <Row>
-          <Col md={6}>
-            <figure className="projects__media">
-              <img
-                className="projects__img"
-                src="/img/trowel-snapshot-2.png"
-                alt="snapshot of project website"
-              />
-            </figure>
-          </Col>
-          <Col className="projects__summary">
-            <h2 className="projects__summary-title">Trowel</h2>
-            <p className="projects__summary--blurb">
-              An App for landscapers to look up valuable plant information and
-              organize gardens by their clients.
-            </p>
-            <ul className="projects__summary--list-group">
-              <li className="projects__summary--list-items"> ➢ Logo Design</li>
-              <li className="projects__summary--list-items">
-                {" "}
-                ➢ Front End Development
-              </li>
-              <li className="projects__summary--list-items">
-                {" "}
-                ➢ Back End Development
-              </li>
-            </ul>
-          </Col>
-        </Row>
-      </Container>
+      {spotlight ? (
+        <Container className="projects__cont">
+          <h2 className="projects__h2">Projects</h2>
+          <Row>
+            <Col md={6}>
+              <figure className="projects__media">
+                <img
+                  className="projects__img"
+                  src="/img/trowel-snapshot-2.png"
+                  alt="snapshot of project website"
+                  onClick={goToProject}
+                />
+              </figure>
+            </Col>
+            <Col className="projects__summary">
+              <h2 className="projects__summary-title">Trowel</h2>
+              <p className="projects__summary--blurb">
+                An App for landscapers to look up valuable plant information and
+                organize gardens by their clients.
+              </p>
+              <ul className="projects__summary--list-group">
+                <li className="projects__summary--list-items">
+                  {" "}
+                  ➢ Logo Design
+                </li>
+                <li className="projects__summary--list-items">
+                  {" "}
+                  ➢ Front End Development
+                </li>
+                <li className="projects__summary--list-items">
+                  {" "}
+                  ➢ Back End Development
+                </li>
+              </ul>
+              <Button
+                className="projects__view-project-btn"
+                onClick={goToProject}
+              >
+                View Project
+              </Button>
+            </Col>
+          </Row>
+        </Container>
+      ) : (
+        <h1>Trowel</h1>
+      )}
     </div>
   );
 }
