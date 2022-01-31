f95e21819d180362226d1f2f5874c8802f01892d
Ben Gross <benrgross@gmail.com>
2022-01-30 19:47:15 -0500

    projects blurb hard coded

--- client/src/pages/Projects.js
+++ client/src/pages/Projects.js
@@ -9,15 +9,30 @@ function Projects() {
         <Row>
           <Col md={6}>
             <figure className="projects__media">
-              <img className="projects__img" src="/img/trowel-snapshot-2.png" />
+              <img
+                className="projects__img"
+                src="/img/trowel-snapshot-2.png"
+                alt="snapshot of project website"
+              />
             </figure>
           </Col>
           <Col className="projects__summary">
             <h2 className="projects__summary-title">Trowel</h2>
-            <p className="projects__summary-blurb">
+            <p className="projects__summary--blurb">
               An App for landscapers to look up valuable plant information and
-              organize gardens by their clients
+              organize gardens by their clients.
             </p>
+            <ul className="projects__summary--list-group">
+              <li className="projects__summary--list-items"> ➢ Logo Design</li>
+              <li className="projects__summary--list-items">
+                {" "}
+                ➢ Front End Development
+              </li>
+              <li className="projects__summary--list-items">
+                {" "}
+                ➢ Back End Development
+              </li>
+            </ul>
           </Col>
         </Row>
       </Container>
